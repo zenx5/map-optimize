097b46bc9c5855d8c253f4077c2555fb770676f0
Octavio Martinez <omartinez1618@gmail.com>
2024-08-07 12:39:53 -0400

    pages

--- src/components/Title.tsx
+++
@@ -1,8 +0,0 @@
-
-
-export default function Title({ title, description }: { title: string, description: string }) {
-    return <div className="max-w-xl mx-auto">
-        <h1 className="text-4xl text-center py-2">{title}</h1>
-        <p className="text-center">{description}</p>
-    </div>
-}
--- src/sections/Home.tsx
+++
@@ -1,6 +0,0 @@
-export default function Home() {
-
-    return <section className="w-screen bg-blue-400 pb-20">
-        <h1 className="text-4xl text-center pt-32">Estrategias para renderizar Marker en mapas</h1>
-    </section>
-}
